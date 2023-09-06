02886d8e31cd840e49af62bed3b0c20a7d521e4c
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2023-09-06 17:04:27 +0200

    Change to node 18

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,15 +12,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://hivehealing.github.io",
+  url: "https://maggie44.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/hivehealing",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "maggie44", // Usually your GitHub org/user name.
-  projectName: "hivehealing.github.io", // Usually your repo name.
+  projectName: "hivehealing", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
